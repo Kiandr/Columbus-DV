b3ed7f248fcc5d0ede0a5320d8256ac343b761cf
K.D.Rad <davoudi.rk@gmail.com>
2016-10-26 14:54:06 -0700

    Initial commit.

---
+++ DV/DVAreaRegisteration.cs
@@ -0,0 +1,60 @@
+﻿/* Copyright (c) 1994-2014 Sage Software, Inc.  All rights reserved. */
+
+using Sage.CA.SBS.ERP.Sage300.VPF.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+
+namespace NND.CA.DV.Web.Areas.VPF
+{
+
+    /// <summary>
+    /// Area Registeration Visual Process Flow
+    /// </summary>
+    public class VPFAreaRegistration : AreaRegistration
+    {
+        /// <summary>
+        /// Area identifier for visual process flow. 
+        /// </summary>
+        public override string AreaName
+        {
+            get
+            {
+                return "DV";
+            }
+        }
+
+        /// <summary>
+        /// Registeratioin of Route config
+        /// </summary>
+        /// <param name="context">the Context</param>
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+
+            RegisterRoutes(context);
+            RegisterBundles();
+
+        }
+        /// <summary>
+        /// URL cunstroction for visual process flow. {tenant}/VPF/{Controller}/{action}/{id}
+        /// </summary>
+        /// <param name="context">Context object</param>
+        private void RegisterRoutes(AreaRegistrationContext context)
+        {
+            context.MapRoute("Core_DV", url: "DV/{controller}/{action}/{id}", 
+                defaults: new { area = "DV", controller = "DV", action = "Index", id = UrlParameter.Optional }, 
+                namespaces: new[] { "NND.CA.DV.Web.Controllers" });
+
+        }
+
+        /// <summary>
+        /// Register bundles
+        /// </summary>
+        private void RegisterBundles()
+        {
+            DVBundleRegisteration.RegisterBundles(BundleTable.Bundles);
+        }
+
+    }
+
+
+}
---
+++ DV/DVBundleRegisteration.cs
@@ -0,0 +1,40 @@
+﻿/* Copyright (c) 1994-2014 Sage Software, Inc.  All rights reserved. */
+#region libraries
+using System.Web.Optimization;
+#endregion
+
+namespace NND.CA.DV.Web
+{
+    /// <summary>
+    /// Bndle Registeratin
+    /// </summary>
+    public class DVBundleRegisteration
+    {
+        /// <summary>
+        /// Bundle Registeration
+        /// </summary>
+        /// <param name="bundles"> Bundles to Javascript libraries</param>
+        internal static void RegisterBundles(BundleCollection bundles)
+        {
+
+            //Bundle registeration for visual process flow. Primarly used to bundle javascript codes into the views
+            #region NND.CA.DV.Web.VPF
+            /*  bundles.Add(new ScriptBundle("~/bundles/Flow").Include(
+                "~/Areas/VPF/Scripts/Flow/Sage.CA.SBS.ERP.Sage300.VPF.FlowRepository.js",
+                "~/Areas/VPF/Scripts/Flow/Sage.CA.SBS.ERP.Sage300.VPF.FlowBehaviour.js"
+               
+           ));*/
+            #endregion
+
+
+            // css holder for visual process flow 
+            #region CSS DV
+            //     bundles.Add(new StyleBundle("~/Style/Flow").Include(
+            //     "~/Areas/VPF/Scripts/svg-edit/Style/VPF.css"));
+            #endregion
+
+        }
+
+
+    }
+}
