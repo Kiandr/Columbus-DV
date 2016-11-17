1bf87502b81f1361a387830dcfd23c870b287656
K.D.Rad <davoudi.rk@gmail.com>
2016-11-17 13:49:28 -0800

    some thouths

--- DV/Controllers/ControllerInternal/DVControllerInternal.cs
+++ DV/Controllers/ControllerInternal/DVControllerInternal.cs
@@ -1,35 +1,30 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.UI.WebControls;
+﻿using NND.CA.Common.Model;
 using NND.CA.Common.Web;
 using NND.CA.DV.Models;
 using NND.CA.DV.Models.ViewModels;
 
 namespace NND.CA.DV.Web.Controllers.ControllerInternal
 {
-    public class DVControllerInternal<T> : MultitenantControllerBase<T> where T : DVModelViewModel<DVModel>, new()
+    public class DvControllerInternal<T> : InternalControllerBase<T> where T : DVModelViewModel<DVModel>, new()
     {
         #region Constructor
 
-        /// <summary>
-        /// Constructor to initialize the service from the input context
-        /// </summary>
-        /// <param name="context"></param>
-        public DVControllerInternal(NND.CA.Common.Model.Context context)
-            //: base(context)
+        public DvControllerInternal(BaseModelContext baseModelContext)
+            : base(baseModelContext)
         {
-            //_orderEntryPaymentInfoCacheKey = CreateSessionKey<TOrder>(OrderEntry.SessiontOrderPaymentInfo);
         }
 
         #endregion
 
         #region Internal Classes
-        internal DVModelViewModel<DVModel> load()
+
+        internal T Load()
         {
-            return new DVModelViewModel<DVModel>();
+            // return serviceLyer.LoadGrid<DVModel>(id);
+            // 
+            return ServicesInLayer;
         }
+
         #endregion
     }
 }
--- DV/Controllers/DVController.cs
+++ DV/Controllers/DVController.cs
@@ -1,77 +1,74 @@
 ﻿/* Copyright (c) 2016 KDR Software, Inc.  All rights reserved. */
 
 #region Namespace
-
 using System.Web.Mvc;
+using System.Web.Routing;
 using Microsoft.Practices.Unity;
-using Microsoft.WindowsAzure.Storage.Core;
+using NND.CA.Common.Model;
 using NND.CA.Common.Web;
 using NND.CA.DV.Models;
-using NND.CA.Common.Model;
 using NND.CA.DV.Models.ViewModels;
-using NND.CA.DV.Web.Controllers.ControllerInternal;
-using JsonNetResult = NND.CA.Common.Model.JsonNetResult;
+using NND.CA.DV.Web.Constants;
 
-
-// ReSharper disable Mvc.PartialViewNotResolved line 25. The partial View has been hardcoded and all of them are being deployed to "NND.CA.Web" via a RoboCop post build srcipt located in the Utility Framework. 
 #endregion
 
 namespace NND.CA.DV.Web.Controllers
 {
     /// <summary>
-    /// Main Controller for Visual Process Flow
+    ///     Main Controller for Visual Process Flow
     /// </summary>
     /// <typeparam name="T">Flow Model</typeparam>
     //public class DVController: Controller
     public class DvController<T> : MultitenantControllerBase<T> where T : DVModelViewModel<DVModel>, new()
     {
-        #region Public Variables
-        //DVControllerInternal<DVModelViewModel<DVModel>> ControllerInternal ;
-        // Retrun Serialized JSON object from index object bind
-        #endregion
-
         #region Constructor
 
-        //public DvController(IUnityContainer container)
-        //    : base(container, ScreenName.DV)
-      
-        //{
-        //}
+        public DvController(IUnityContainer container)
+            : base(container, ScreenName.DV)
 
-
+        {
+        }
 
         #endregion
 
         #region Initialize MultitenantControllerBase
 
         /// <summary>
-        /// Initializes the specified request context.
+        ///     Initializes the specified request context.
         /// </summary>
         /// <param name="requestContext">The request context.</param>
-        protected override void Initialize(System.Web.Routing.RequestContext requestContext)
+        protected override void Initialize(RequestContext requestContext)
         {
             base.Initialize(requestContext);
             //ControllerInternal = new DVControllerInternal<DVModelViewModel<DVModel>>(requestContext);
         }
+
+        #endregion
+
+        #region Public Variables
+
+        //DVControllerInternal<DVModelViewModel<DVModel>> ControllerInternal ;
+        // Retrun Serialized JSON object from index object bind
+
         #endregion
 
         #region Public Actions
+
         [HttpGet]
         public ActionResult Index(string id)
         {
-            return PartialView(Constants.DvConstant.DvIndexPartialViewPath);
+            return PartialView(DvConstant.DvIndexPartialViewPath);
         }
 
         [HttpGet]
         public virtual string Load(string id)
         {
-            return ("rest");
+            return "rest";
 
             // var t = controllerInternal.load();
             //var  tt =t.Data;
-
-
         }
-    #endregion
+
+        #endregion
     }
 }
